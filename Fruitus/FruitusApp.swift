416a005fb9e91fc59ea56c07f44aafdd39294d71
Chitraarasu <98887175+chitraarasu-kirshi@users.noreply.github.com>
2023-04-12 12:26:14 +0530

    Initial Commit

---
+++ Fruitus/FruitusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitusApp.swift
+//  Fruitus
+//
+//  Created by kirshi on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
